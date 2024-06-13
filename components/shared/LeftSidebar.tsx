46d6dcafded7f3724bc02ec3cf315cfc7b953a4e
clntt <c.connery14@gmail.com>
2024-06-13 01:49:51 +0100

    leftsidebar pathname correction

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -8,6 +8,7 @@ import { SignedOut } from '@clerk/nextjs';
 import { Button } from '../ui/button';
 
 const LeftSideBar = () => {
+  const pathname = usePathname();
   
   return (
     <section className='background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px] custom-scrollbar'>
@@ -15,7 +16,6 @@ const LeftSideBar = () => {
       <div className='flex flex-1 flex-col gap-6'>
        {sidebarLinks.map((item) => {
           
-        const pathname = usePathname();
         const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
 
 
